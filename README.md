b4d187d3eea2dadfaa7a4d58fd5ac3e859dee3cb
glopezv95 <glopezv95@gmail.com>
2024-02-06 10:51:58 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # api-spotify
-A scrypt to retrieve data from the spotify API and generate a csv file with the top 50 songs by country
+A script to retrieve data from the **spotify API** and generate a csv file with the **top 50 songs by country**.
+For the script to work correctly, a **client_id** and **secret_id** must be passed to the following os.environ.get() functions:
+
+```python
+id = os.environ.get('ID')
+secret = os.environ.get('SECRET')
+```
+
+In order to achieve this, the script is looking for a **.env** file outside of the repository. The layout of the repository should be as follows:
+
+```
+ID = "your_custom_client_id"
+CLIENT = "your_custom_client_id"
+```
+
+To get this ids, the Spotify API requires to create a free app in the **Spotify for Developers** webpage (https://developer.spotify.com/).
---
+++ script.py
@@ -0,0 +1,88 @@
+from dotenv import load_dotenv
+import os
+import spotipy
+import pandas as pd
+import numpy as np
+
+env_path = os.path.join(
+    os.path.dirname(__file__),
+    '..',
+    '.env')
+
+load_dotenv(env_path)
+
+id = os.environ.get('ID')
+secret = os.environ.get('SECRET')
+
+manager = spotipy.SpotifyClientCredentials(client_id = id, client_secret = secret)
+sp = spotipy.Spotify(client_credentials_manager = manager)
+
+top50_countries = {
+    'spain': '37i9dQZEVXbNFJfN1Vw8d9',
+    'japan': '37i9dQZEVXbKXQ4mDTEBXq',
+    'usa': '37i9dQZEVXbLRQDuF5jeBp',
+    'morocco': '37i9dQZEVXbJU9eQpX8gPT',
+    'brasil': '37i9dQZEVXbMXbN3EUUhlg',
+    'italy': '37i9dQZEVXbIQnj7RRhdSX',
+    'argentina': '37i9dQZEVXbMMy2roB9myp',
+    'peru': '37i9dQZEVXbJfdy5b0KP7W',
+    'india': '37i9dQZEVXbLZ52XmnySJg',
+    'philippines':'37i9dQZEVXbNBz9cRCSFkY'}
+
+final_df = pd.DataFrame()
+
+for country, country_id in top50_countries.items():
+
+    tracks = sp.playlist_items(playlist_id = country_id)
+
+    artists_id = []
+
+    for item in tracks['items']:
+        artists_id.append(item['track']['artists'][0]['id'])
+        
+    df = []
+
+    for item in tracks['items']:
+        track = item['track']
+        df.append({
+            'name':track['name'],
+            'main_artist':track['artists'][0]['name'],
+            'release_date':track['album']['release_date'],
+            'duration_ms':track['duration_ms'],
+            'popularity':track['popularity'],
+            'explicit':track['explicit'],
+            'album_type':track['album']['album_type'],
+            'track_id':track['id'],
+            'main_artist_id':track['artists'][0]['id']
+        })
+        
+    df = pd.DataFrame(df)
+    len(df)
+
+    artists_genre = []
+
+    for id in artists_id:
+        artist = sp.artist(artist_id = id)
+        if len(artist['genres']) >= 1:
+            artists_genre.append({'artist_id':id,'genre':artist['genres'][0]})
+        else:
+            artists_genre.append({'artist_id':id,'genre':np.nan})
+            
+    genre_df = pd.DataFrame(artists_genre)
+
+    len(genre_df)
+
+    df = df.merge(
+        right = genre_df,
+        left_on = 'main_artist_id',
+        right_on = 'artist_id',
+        how = 'left',
+        ).drop('artist_id', axis = 1).drop_duplicates().reset_index(drop = True)
+    
+    df['list_country'] = country
+    df['ranking'] = range(1, len(df) + 1)
+    
+    final_df = pd.concat([final_df, df], axis = 0, ignore_index = True)
+    
+final_df.to_csv(
+    path_or_buf = os.path.join(os.path.dirname(__file__), 'spotify_data.csv'))
